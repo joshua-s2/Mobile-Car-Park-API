0cf5d0f996c95887901ef13a896ad5b062f63226
gabrielandy <andikan@sure.ng>
2019-10-29 09:21:02 +0100

    Added car park db schema

---
+++ database/migrations/2019_10_29_080226_create_car_parks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarParksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_parks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('owner', 50);
+            $table->string('address');
+            $table->string('tel', 15);
+            $table->bigInteger('fee');
+            $table->string('image_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_parks');
+    }
+}
