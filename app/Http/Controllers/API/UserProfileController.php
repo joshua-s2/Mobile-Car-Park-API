53e7756622a3fb7e8802c4aafd0a5220d7c665ae
emmannl <eul.nelson@gmail.com>
2019-10-29 16:20:20 +0100

    update user profile management

--- app/Http/Controllers/API/UserProfileController.php
+++ app/Http/Controllers/API/UserProfileController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\API;
 
 
 use App\Classes\Helper;
-use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserProfileController
 {
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = auth()->user();
+    }
+
     public function show()
     {
         return response()->json([
@@ -20,14 +27,14 @@ class UserProfileController
     public function update(Request $request)
     {
         $data = $request->validate([
-            'phone' => ['required', 'phone:NG'],
+            'phone' => ['required', 'phone:NG', Rule::unique('users')->ignore($this->user->id)],
             'first_name' => ['required', 'string', 'min:3', 'max:255'],
             'last_name' => ['required', 'string', 'min:3', 'max:255'],
             'email' => ['nullable', 'email', 'max:255'],
         ]);
 
         try {
-            User::updated($data);
+            $this->user->updated($data);
 
             return response()->json(['message' => 'Profile updated', 'data' => $data]);
         } catch (\Exception $e) {
