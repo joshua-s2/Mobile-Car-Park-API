ccadf7fada144a43febe12cf6341be7b3217d7ec
gabrielandy <andikan@sure.ng>
2019-10-29 10:26:18 +0100

    Updated car park controller

--- app/Http/Controllers/API/CarParkController.php
+++ app/Http/Controllers/API/CarParkController.php
@@ -26,4 +26,46 @@ class CarParkController extends Controller
     {
         $this->user = auth()->user();
     }
+
+    /**
+     * Add a parking space to the database
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    	// Validate requests
+    	$this->validate($request, [
+            'name'		  => ['required', 'regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/'],
+            'owner'		  => ['required', 'regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/'],
+            'address'	  => 'required|string',
+            'tel'		  => 'required|string',
+            'fee'		  => 'required|integer|min:0',
+            'image_link'  => 'string|nullable',
+        ]);
+
+        $park = new CarPark;
+
+        $park->name 	  = $request->name;
+        $park->owner 	  = $request->owner;
+        $park->address 	  = $request->address;
+        $park->tel 		  = $request->tel;
+        $park->fee 		  = $request->fee;
+        $park->image_link = $request->image_link;
+
+        // Save to db
+        if ($park->save()) {
+            return response()->json([
+                'status'  => true,
+                'result'  => $park,
+                'message' => 'Car Park was successfully added'
+            ], 200);
+        }
+        else {
+            return response()->json([
+                'status'  => false,
+                'message' => 'An error occurred'
+            ], 501);
+        }
+    }
 }
