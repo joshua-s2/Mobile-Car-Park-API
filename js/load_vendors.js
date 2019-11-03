0d4827f5698c5c5521a2e3ac8443850d119603d0
emmannl <eul.nelson@gmail.com>
2019-11-03 16:27:57 +0100

    FE layout

---
+++ js/load_vendors.js
@@ -0,0 +1,12 @@
+/**
+ * All vendor/third party JS fis go in here
+ * Order is of importance, so BE CAREFUL BEFORE ALTERATION
+ */
+// JQuery
+document.write(`<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>`);
+
+// Pooper,js
+document.write(`<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>`);
+
+// Bootstrap
+document.write(`<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>`);
